cebc6081895ad9b6e4a3d1605e3638db9f4aa592
Dinar Davis <dinardavis@gmail.com>
2022-09-23 20:09:56 +0300

    Update launch images and captions

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-document.querySelector('.background-1').style.backgroundImage = "url('/imgs/starlink1.png')" 
-document.querySelector('.background-2').style.backgroundImage = "url('/imgs/crs-25_1.png')" 
+document.querySelector('.background-1').style.backgroundImage = "url('/imgs/starlink.png')" 
+document.querySelector('.background-2').style.backgroundImage = "url('/imgs/tmobile_update.png')" 
 document.querySelector('.background-3').style.backgroundImage = "url('/imgs/starship.png')" 
 document.querySelector('.background-4').style.backgroundImage = "url('/imgs/starship_landing.png')" 
 
