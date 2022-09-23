8a3f66a978ee109e4e5add61a743b097b48e4fbe
Dinar Davis <dinardavis@gmail.com>
2022-09-23 19:51:00 +0300

    initialize repo with spacex_clone files

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelector('.background-1').style.backgroundImage = "url('/imgs/starlink1.png')" 
+document.querySelector('.background-2').style.backgroundImage = "url('/imgs/crs-25_1.png')" 
+document.querySelector('.background-3').style.backgroundImage = "url('/imgs/starship.png')" 
+document.querySelector('.background-4').style.backgroundImage = "url('/imgs/starship_landing.png')" 
+
