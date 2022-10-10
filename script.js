dfb9f4c4e8f6906b7634c934908d074fef9cea40
Dinar Davis <dinardavis@gmail.com>
2022-10-10 16:43:46 +0300

    Update launch images; refactor background images/adding to css; adjust nav and button styling

--- script.js
+++ script.js
@@ -1,5 +1,2 @@
-document.querySelector('.background-1').style.backgroundImage = "url('/imgs/starlink.png')" 
-document.querySelector('.background-2').style.backgroundImage = "url('/imgs/tmobile_update.png')" 
-document.querySelector('.background-3').style.backgroundImage = "url('/imgs/starship.png')" 
-document.querySelector('.background-4').style.backgroundImage = "url('/imgs/starship_landing.png')" 
+
 
